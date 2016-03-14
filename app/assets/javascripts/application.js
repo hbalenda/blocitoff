6ad1255b710bd014b3caab69c11cbce1c42aa574
hcbviolet <hcbviolet@gmail.com>
2016-03-13 17:29:21 -0700

    Add Blocmetrics analytics javascript snippet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,16 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+var blocmetrics = {};
+  blocmetrics.report = function(eventName) {
+    //Create an event JSON object to send to analytics service.
+    var event = {event: { name: eventName }};
+    // Initialize a new XMLHttpRequest aka AJAX request.
+    var request = new XMLHttpRequest();
+    // Set HTTP method to POST/ the url of events endpoint, and allow the request to run asynchronously.
+    request.open("POST", "http://localhost:3000/api/events", true);
+    // Tells API::EventsController to process the request as JSON.
+    request.setRequestHeader('Content-Type', 'application/json');
+    // Send request
+    request.send(JSON.stringify(event));
+  };
